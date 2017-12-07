c41033d951ef9d1695753c94660391e5b53a7994
Eric C Schoenborn <cultureondemand@gmail.com>
2017-12-07 17:33:07 -0500

    Update scrolly.js

--- assets/js/scrolly.js
+++ assets/js/scrolly.js
@@ -308,7 +308,7 @@ var range = [];
  var x = d3.scale.linear()
     .range([0, width])
 /////////////////////     .domain([0, data.length -1]);
-   .domain([2000, 2050]);
+   .domain([2000, 2014]);
 
     
 /*    var x = d3.scale.ordinal()
