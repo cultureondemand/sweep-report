f7e6c5977b7de8d57fb19c73122c1d9e13bd121b
Eric C Schoenborn <cultureondemand@gmail.com>
2017-12-08 10:36:08 -0500

    sctterplot layer

--- assets/js/scrolly.js
+++ assets/js/scrolly.js
@@ -374,6 +374,14 @@ svg.append("g")
       .attr("class", "line")
       .attr("d", line);
 
+    // Add the scatterplot
+    svg.selectAll("dot")
+        .data(data)
+      .enter().append("circle")
+        .attr("r", 3.5)
+        .attr("cx", function(d, i) { return x(i); })
+        .attr("cy", function(d) { return y(d); });
+
   
   }
 
