7e14a851c285563cf28b6f0891b8099b49625c03
Eric C Schoenborn <cultureondemand@gmail.com>
2017-12-15 12:17:11 -0500

    Update scrolly.js

--- assets/js/scrolly.js
+++ assets/js/scrolly.js
@@ -419,7 +419,8 @@ svg.append("g")
       // draw x axis with labels and move to the bottom of the chart area
         svg.append("g")
             .attr("class", "xaxis axis")  // two classes, one for css formatting, one for selection below
-            .attr("transform", "translate(0," + (height - padding) + ")")
+   /////         .attr("transform", "translate(0," + (height - padding) + ")")
+            .attr("transform", "translate(0," + (height) + ")")
             .call(xAxis);
             
   // draw y axis with labels and move in from the size by the amount of padding
