9e59527979b055bee1fe7f9c28341c6447730190
Eric C Schoenborn <cultureondemand@gmail.com>
2017-12-20 10:44:01 -0500

    Update scrolly.js

--- assets/js/scrolly.js
+++ assets/js/scrolly.js
@@ -593,10 +593,11 @@ svg.append("g")
     
 
  .on("mousemove", function(d){
-            div.style("left", d3.event.pageX-23+"px");
-            div.style("top", (d3.event.pageY)-45+"px");
+      /*      div.style("left", d3.event.pageX-23+"px");
+            div.style("top", (d3.event.pageY)-45+"px"); */
             div.style("display", "inline-block");
-            div.style("position", "absolute");
+       ////     div.style("position", "absolute");
+            div.style("position", "fixed");
                         div.attr("class", "fixedtooltip");
 
              div.html("<h4>Denver-Aurora delayed "+(d.value)+" hours annually in "+(d.date)+"</h4>");
